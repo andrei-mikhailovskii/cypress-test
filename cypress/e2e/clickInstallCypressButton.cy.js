c95aa3be0af18ccb572261af383958e980e67d60
Andrei Mikhailovskii <dr.mikhaylovsky@gmail.com>
2023-10-04 18:14:05 +0200

    refactor: add wait for test purposes

--- cypress/e2e/clickInstallCypressButton.cy.js
+++ cypress/e2e/clickInstallCypressButton.cy.js
@@ -1,17 +1,18 @@
-Cypress._.times(100, () => {
+Cypress._.times(30, () => {
 describe('Click NPM Install Cypress button', () => {
 
     beforeEach(() => {
 
-        cy.intercept('POST', 'https://cloud.cypress.io/graphql').as('graphql');
         cy.visit('/');
-        cy.wait('@graphql');
+
+        cy.wait(3000); 
 
         cy.get('[aria-label="Cookie Consent Banner"]').then((banner) => {
             if (banner.is(':visible')) {
                 cy.contains('Reject All').click();
             }
         });
+        //cy.wait(1000); 
     
     });
 
@@ -20,6 +21,8 @@ describe('Click NPM Install Cypress button', () => {
         cy.get('button:contains("npm install cypress")')
         .should('be.visible')
         .click();
+
+        //cy.wait(1000); 
 
         cy.get('[data-cy="modal-install-copy"]')
         .should('be.visible')
